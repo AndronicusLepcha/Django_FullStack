993caa59e36e1c445361b7471405ad8477bc3951
Robot <androlepcha843@gmail.com>
2023-11-25 21:52:09 +0600

    Added Drafts and edit in views

--- Blog_Project/myblog/blog/urls.py
+++ Blog_Project/myblog/blog/urls.py
@@ -3,4 +3,10 @@ from blog import views
 
 urlpatterns=[
     path('about/',views.AboutView().as_view(),name='about'),
+    path('',views.PostListView(),name='post_list'),
+    path('post/(?P<pk>\d+)',views.PostDetialView.as_view(),name='post_detail'),
+    path('post/new/',views.CreatePostView.as_view(),name='post_new'),
+    path('post/(?P<pk>\d+)/edit/',views.PostUpdateView.as_view(),name='post_edit'),
+    path('post/(?P<pk>\d+)/remove/',views.PostDeleteView.as_view(),name='post_remove'),
+    path('drafts/',views.DraftListView.as_view(),name='post_draft_list'),
 ]
--- Blog_Project/myblog/blog/views.py
+++ Blog_Project/myblog/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from blog.models import Post,Comment
-from django.views.generic import (TemplateView)
+from blog.forms import PostForm,CommentForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.views.generic import (TemplateView,DetailView,CreateView,UpdateView,DeleteView)
 
 # Create your views here.
 
@@ -11,4 +15,32 @@ class PostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+class PostDetialView(DetialView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'block/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'block/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin,ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
